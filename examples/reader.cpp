1617e04e59b489d44fd50970c551bc2b569a1f39
Thomas Vincent <thomas.vincent@univ-nantes.fr>
2015-09-10 22:33:46 +0200

    Follow changes of BVect

--- examples/reader.cpp
+++ examples/reader.cpp
@@ -70,8 +70,8 @@ int main (int argc, char** argv){
     vector<BVect> solutions = solver.getSols();
     cout << "The problem has " << solutions.size() << " non dominated points:" << endl;
     for(vector<BVect>::iterator solution = solutions.begin(); solution != solutions.end(); solution++) {
-      cout << "\t" << solution->_y1 << " " << solution->_y2 << " | " 
-        << solution->_x.at(0) << " " << solution->_x.at(1) << endl;
+      cout << "\t" << solution->y1() << " " << solution->y2() << " | " 
+        << solution->x().at(0) << " " << solution->x().at(1) << endl;
     }
   }
 }
